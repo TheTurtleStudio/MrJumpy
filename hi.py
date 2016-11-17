278e6e8fc77e7e8cd89901ed5cd466047e2d38e3
Brayden <braydenlynnpeterson@gmail.com>
2016-11-16 22:12:23 -0700

    11/16/2016 *Added speed-control using sleep module *Added continous movement for all directions

--- hi.py
+++ hi.py
@@ -1,4 +1,5 @@
 import pygame
+from time import sleep as wait
 
 pygame.init()
 
@@ -23,30 +24,93 @@ y=you.rect.center[1]
 You = pygame.sprite.RenderClear(you)
 gameExit = False
 
+class Dir():
+    def __init__(self,val):
+        self.val = val
+    def getvalue(self):
+        return self.val
+    def setvalue(self,val):
+        self.val = val
+        
+move_left=Dir(False)
+move_right=Dir(False)
+move_up=Dir(False)
+move_down=Dir(False)
+
+
+
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                old_x = you.rect.center[0]
-                old_y = you.rect.center[1]
-                x=old_x - 10
-                y=old_y
-                you.rect.center=(x,y)
+                move_left.setvalue(True)
+                #old_x = you.rect.center[0]
+                #old_y = you.rect.center[1]
+                #x=old_x - 10
+                #y=old_y
+                #you.rect.center=(x,y)
+            
+            
             elif event.key == pygame.K_RIGHT:
-                old_x = you.rect.center[0]
-                old_y = you.rect.center[1]
-                x=old_x + 10
-                y=old_y
-                you.rect.center=(x,y)
+                move_right.setvalue(True)
+                #old_x = you.rect.center[0]
+                #old_y = you.rect.center[1]
+                #x=old_x + 10
+                #y=old_y
+                #you.rect.center=(x,y)
+                pass
+            elif event.key == pygame.K_UP:
+                move_up.setvalue(True)
+                #old_x = you.rect.center[0]
+                #old_y = you.rect.center[1]
+                #x=old_x
+                #y=old_y - 10
+                #you.rect.center=(x,y)
+                pass
+            elif event.key == pygame.K_DOWN:
+                move_down.setvalue(True)
+                #old_x = you.rect.center[0]
+                #old_y = you.rect.center[1]
+                #x=old_x
+                #y=old_y + 10
 
-    #pygame.draw.rect(gameDisplay, white, [380,30,40,40])
-    
-    #gameDisplay.blit(you,(380,30))
+                #you.rect.center=(x,y)
+                pass
+            
+        if event.type == pygame.KEYUP:
+            for direction in (move_left,move_right,move_up,move_down):
+                direction.setvalue(False)
+                
+    if move_left.getvalue():
+        old_x = you.rect.center[0]
+        old_y = you.rect.center[1]
+        x=old_x - 5
+        y=old_y
+        you.rect.center=(x,y)
+    if move_right.getvalue():
+        old_x = you.rect.center[0]
+        old_y = you.rect.center[1]
+        x=old_x + 5
+        y=old_y
+        you.rect.center=(x,y)
+    if move_up.getvalue():
+        old_x = you.rect.center[0]
+        old_y = you.rect.center[1]
+        x=old_x
+        y=old_y - 5
+        you.rect.center=(x,y)
+    if move_down.getvalue():
+        old_x = you.rect.center[0]
+        old_y = you.rect.center[1]
+        x=old_x
+        y=old_y + 5
+        you.rect.center=(x,y)
     You.clear(gameDisplay,background)
     You.draw(gameDisplay)
     pygame.display.update()
+    wait(.05)
 
 pygame.quit()
 quit()
